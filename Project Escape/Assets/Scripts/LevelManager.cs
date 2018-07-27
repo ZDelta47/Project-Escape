8b690381194c8575a591268f71ceb6e367a6a334
ZiCKeZaCKe90 <38163179+ZiCKeZaCKe90@users.noreply.github.com>
2018-07-27 12:06:45 +0200

    Update #01
    - Added GameManager, SoundManager, UIManager and LevelManager
    - Added Loader to load all Managers on Awake()
    - Added Pause Menu (Esc) with options to Resume (Esc) and Restart (R)
    - Added WaterSplashFX (Particle Effect when something fallsinto the water)

--- Project Escape/Assets/Scripts/LevelManager.cs
+++ Project Escape/Assets/Scripts/LevelManager.cs
@@ -5,8 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+    public static LevelManager instance = null;
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
     public void ChangeScene(int index)
     {
         SceneManager.LoadScene(index);
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
---
+++ Project Escape/Assets/Scripts/UIManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager instance = null;
+    public GameObject pauseMenu;
+
+
+    public bool isPaused;
+
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+
+        }
+
+        if (isPaused)
+        {
+            ActivatePauseMenu();
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                LevelManager.instance.RestartScene();
+                DeactivatePauseMenu();
+                isPaused = false;
+            }
+        }
+        else
+        {
+            DeactivatePauseMenu();
+        }
+
+    }
+
+    void ActivatePauseMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+
+    }
+
+    void DeactivatePauseMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+
+    }
+}
